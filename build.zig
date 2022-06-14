86b8537e38373cca796092de7ef9f8f1960657be
Hanna <hanna@pyxel.one>
2022-06-14 06:47:49 -0400

    build: add run step to run kernel in qemu

--- build.zig
+++ build.zig
@@ -13,6 +13,25 @@ pub fn build(b: *std.build.Builder) void {
     kernel.addAssemblyFile("src/asm/boot.s");
     kernel.addAssemblyFile("src/asm/trap.s");
 
+    // zig fmt: off
+    const qemu_args = &[_][]const u8{
+        "qemu-system-riscv64",
+        "-d", "guest_errors,unimp",
+        "-kernel", "zig-out/bin/kernel",
+        "-machine", "virt",
+        "-bios", "none",
+        "-cpu", "rv64",
+        "-m", "128M",
+        "-smp", "4",
+    // zig fmt: on
+    };
+
+    const run_cmd = b.addSystemCommand(qemu_args);
+    run_cmd.step.dependOn(&kernel.step);
+
+    const run_step = b.step("run", "run the kernel in qemu");
+    run_step.dependOn(&run_cmd.step);
+
     kernel.setTarget(target);
     kernel.setBuildMode(mode);
     kernel.code_model = .medium;
